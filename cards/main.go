88866ac76a7b02dcba8526d247c0d7c04322fd21
NachoMol <ignaciotmolina@gmail.com>
2023-05-05 19:20:47 -0300

    implementando newDeckfromFile en el main

--- cards/main.go
+++ cards/main.go
@@ -2,12 +2,16 @@ package main
 
 func main() {
 
-	cards := newDeck()
-	cards.saveToFile("my_cards")
-	deckFromFile("my_cards")
+	// cards := newDeck()
+	// cards.saveToFile("my_cards")
+	// deckFromFile("my_cards")
 
 	// hand, remainingCards := deal(cards, 5)
 
 	// hand.print()
 	// remainingCards.print()
+
+	cards := deckFromFile("my_cards")
+	cards.print()
+
 }
