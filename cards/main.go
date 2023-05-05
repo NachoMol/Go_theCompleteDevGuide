22b098c4278b599b53d32aaa873db38cc80126f5
NachoMol <ignaciotmolina@gmail.com>
2023-05-05 14:47:46 -0300

    subiendo funcion saveTofile()

--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
 
 	cards := newDeck()
+	cards.saveToFile("my_cards")
 
 	// hand, remainingCards := deal(cards, 5)
 
 	// hand.print()
 	// remainingCards.print()
-
-	fmt.Println(cards.toString())
-
 }
