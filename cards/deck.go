e9970c1e7ce30141e5dae410d03a7286fe82ab70
NachoMol <ignaciotmolina@gmail.com>
2023-09-16 18:22:57 -0300

    modificando seed para randomizar cada secuencia

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of deck
@@ -58,8 +59,11 @@ func deckFromFile(filename string) deck {
 
 func (d deck) shuffle() {
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
 
+		source := rand.NewSource(time.Now().UnixNano())
+		r := rand.New(source)
+
+		newPosition := r.Intn(len(d) - 1)
 		d[i], d[newPosition] = d[newPosition], d[i]
 
 	}
