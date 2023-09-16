5feb19a4655e99f18fc7fd85df91f69602bad59e
NachoMol <ignaciotmolina@gmail.com>
2023-09-16 18:06:07 -0300

    subiendo randomizador de cartas

--- cards/main.go
+++ cards/main.go
@@ -2,16 +2,8 @@ package main
 
 func main() {
 
-	// cards := newDeck()
-	// cards.saveToFile("my_cards")
-	// deckFromFile("my_cards")
-
-	// hand, remainingCards := deal(cards, 5)
-
-	// hand.print()
-	// remainingCards.print()
-
-	cards := deckFromFile("my_cards")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 
 }
